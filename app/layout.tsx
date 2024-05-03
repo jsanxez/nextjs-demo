eb6b818942e998bed59e9551c1a87d406e279db7
Jose Sanchez <jsanxez7@gmail.com>
2024-05-02 20:48:18 -0500

    feat: add styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import '@/app/ui/global.css'
+export default function RootLayout({ children, }: { children: React.ReactNode; }) {
   return (
     <html lang="en">
       <body>{children}</body>
